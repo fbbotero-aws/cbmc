16cfd92dbaaf714efeb19d92c55487f6b1fdff36
Fotis Koutoulakis <fotis@hey.com>
2020-08-10 17:09:19 +0100

    Attempt #3

--- .github/workflows/vs2019/run-tests.bat
+++ .github/workflows/vs2019/run-tests.bat
@@ -1,5 +1,8 @@
 @REM Run tests on visual studio 2019 generated build
 
+@echo "Set up PATH to find Visual Studio tools"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
 @echo PATH:
 @echo
 @echo %PATH%
